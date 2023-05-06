591069145a424d152b487a6e66584276cfca2847
Mojetioluwa EWETOLA <78496672+Mojetioluwa@users.noreply.github.com>
2023-05-06 09:07:17 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+    },
+  },
+  plugins: [],
+}
+
